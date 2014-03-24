ab51bc5d7835af1c505675626a5aa1cb66ea6f61
Andrew <sautin1@yandex.ru>
2014-03-24 04:00:21 +0400

    1t5p. HashTable. Working version. Commit 2

--- 1t5p_hashtable/hashtable.h
+++ 1t5p_hashtable/hashtable.h
@@ -3,16 +3,17 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <stdexcept>
+#include <string>
 
 template <class KeyT, class ValueT>
 class HashTable {
 public:
-	typedef std::pair<const KeyT, ValueT> TListElement;
+    typedef std::pair<KeyT, ValueT> TListElement;
 	
 	class Iterator
 	{
-	public: 
-		//Iterator(){}
+    public:
 
         Iterator(const HashTable& parent, const Iterator& copy_it): parent_(parent), bucket_it_(copy_it.bucket_it_), list_it_(copy_it.list_it_){}
 
@@ -21,70 +22,102 @@ public:
 				 const typename std::list<TListElement>::iterator& list_it)
 				: parent_(parent), bucket_it_(bucket_it), list_it_(list_it){}
 
-        TListElement* operator ->()
+        TListElement* operator->()
 		{
             return &(*list_it_);
 		}
 
-		operator const Iterator* ()
-		{
-			return this;
-		}
+        const TListElement* operator->() const
+        {
+            return &(*list_it_);
+        }
 
-        ValueT& operator *() const {
+        const ValueT& operator*() const
+        {
             return list_it_->second;
 		}
-		
-		Iterator operator =(const Iterator& copy_it)
+        ValueT& operator*()
+        {
+            return list_it_->second;
+        }
+
+        Iterator operator=(const Iterator& copy_it)
 		{
-			Iterator it;
-			it.list_it_ = copy_it.list_it_;
-			it.bucket_it_ = copy_it.bucket_it_;
-			return it;
+            //Iterator it(parent_, copy_it.bucket_it_, copy_it.list_it_);
+            this->bucket_it_ = copy_it.bucket_it_;
+            this->list_it_ = copy_it.list_it_;
+            return *this;
 		}
 
-		Iterator& operator ++()
+        Iterator& operator++()
 		{
-			++list_it_;
-			while (list_it_ == bucket_it_ -> end()){
-				++bucket_it_;
-				list_it_ = bucket_it_ -> begin();
-			}
+            ++list_it_;
+            if (list_it_ == bucket_it_->end()){
+                if (bucket_it_ == parent_.end().bucket_it_){
+                    //*this points to the end of hashtable_
+                    return *this;
+                }
+                ++bucket_it_;
+                while (bucket_it_ != parent_.end().bucket_it_ && bucket_it_->empty()){
+                    ++bucket_it_;
+                }
+                if (bucket_it_->empty()){
+                    list_it_ = parent_.end().list_it_;
+                }
+                else {
+                    list_it_ = (*bucket_it_).begin();
+                }
+            }
 			return *this;
 		} //prefix form
-		
-		Iterator& operator --()
+
+        Iterator& operator--()
 		{
-			--list_it_;
-			while (list_it_ == bucket_it_ -> begin()){
-				--bucket_it_;
-				list_it_ = bucket_it_ -> end();
-			}
-			return *this;
+            if (list_it_ == bucket_it_->begin()){
+                if (bucket_it_ == parent_.begin().bucket_it_){
+                    //*this points to the begin of hashtable_
+                    return *this;
+                }
+                --bucket_it_;
+                while (bucket_it_ != parent_.begin().bucket_it_ && bucket_it_->empty()){
+                    --bucket_it_;
+                }
+                if (bucket_it_->empty()){
+                    list_it_ = parent_.begin().list_it_;
+                }
+                else {
+                    list_it_ = (*bucket_it_).end();
+                }
+            }
+            else {
+                --list_it_;
+            }
+            return *this;
 		} //prefix form
 		
-		const Iterator operator ++(int)
+        const Iterator operator++(int)
 		{
 			Iterator old = *this;
 			operator ++();
 			return old;
 		} //postfix form
 
-		const Iterator operator --(int)
+        const Iterator operator--(int)
 		{
 			Iterator old = *this;
 			operator --();
 			return old; 
 		} //postfix form
 
-		bool operator == (const Iterator &equal_it)
+        bool operator==(const Iterator &equal_it) const
 		{
 			return (list_it_ == equal_it.list_it_) && (bucket_it_ == equal_it.bucket_it_);
 		}
 
-		bool operator != (const Iterator &inequal_it)
+        bool operator!=(const Iterator &inequal_it) const
 		{
-            return (list_it_ != inequal_it.list_it_) || (bucket_it_ != inequal_it.bucket_it_);
+            return !((*this) == inequal_it);
+            //return (list_it_ != inequal_it.list_it_) || (bucket_it_ != inequal_it.bucket_it_);
 		}
 
         const HashTable& parent_;
@@ -92,18 +125,17 @@ public:
         typename std::list<TListElement>::iterator list_it_;
 	};
 
-	HashTable(size_t buckets_quantity)
-    :hashtable_(buckets_quantity, std::list<TListElement>()),
-      it_begin_(*this, hashtable_.begin(), hashtable_.front().begin()),
-      it_end_(*this, hashtable_.end(), hashtable_.back().end())
-	{
-		/*Creates hashtable_ with buckets_quantity empty lists.
-		Complexity: linear in the buckets_quantity*/
-		update_begin();
-		update_end();
-	}
-
-	~HashTable(){}
+    HashTable(size_t buckets_quantity)
+        :hashtable_(buckets_quantity, std::list<TListElement>()),
+          it_begin_(*this, hashtable_.begin(), hashtable_.front().begin()),
+          it_end_(*this, hashtable_.end(), hashtable_.back().end()),
+          element_quantity_(0)
+    {
+        /*Creates hashtable_ with buckets_quantity empty lists.
+        Complexity: linear in the buckets_quantity*/
+        update_begin();
+        update_end();
+    }
 
 	Iterator begin() const
 	{
@@ -115,48 +147,68 @@ public:
 		return it_end_;
 	}
 
-	Iterator find(const KeyT& key) const
+    bool isEmpty() const
+    {
+        return (element_quantity_ == 0);
+    }
+
+
+    Iterator find(const KeyT& key) const
 	{
 		/*Finds key in hashtable_
 		Complexity: linear in the element_quantity_*/
+        if (isEmpty()){
+            return it_end_;
+        }
         Iterator it(*this, it_begin_);
         for (; it != it_end_; ++it){
-            if ((*it) == key){
+            if (it->first == key){
 				break;
 			}
 		}
-		return it;
+        return it;
 	}
 
-	std::pair<Iterator, bool> insert(const KeyT& key, const ValueT& value)
-	{
-		/*Insert pair {key,value} into hashtable_
-		Complexity: linear in the element_quantity_*/
-		Iterator new_place_it = find(key);
-		if (new_place_it != it_end_){
-			/*found => cannot be inserted*/
-		} else {
-			/*not found => will be inserted*/
-			size_t new_index = hash_function(key);
-			hashtable_[new_index].push_front(std::make_pair(key, value));
-			++element_quantity_;
-			if (element_quantity_ >= rehash_coefficient * hashtable_.size()){
-				rehash(hashtable_.size() + 1);
-			}
-			update_end();
-		}
-		return std::make_pair(new_place_it, new_place_it == it_end_);
-	}
+    std::pair<Iterator, bool> insert(const KeyT& key, const ValueT& value)
+    {
+        /*Insert pair {key,value} into hashtable_
+        Complexity: linear in the element_quantity_*/
+        Iterator new_place_it = it_end_;
+        if (!isEmpty()){
+            new_place_it = find(key);
+            /*not found => it = it_end_*/
+        }
+        bool success = (new_place_it == it_end_);
+        if (success){
+            //Element will be inserted
+            size_t new_index = hash_function(key);
+            hashtable_[new_index].push_front(std::make_pair(key, value));
+            ++element_quantity_;
+            Iterator temp_it(*this, hashtable_.begin() + new_index, hashtable_[new_index].begin());
+            new_place_it = temp_it;
+            update_begin();
+            update_end();
+            if (element_quantity_ >= rehash_coefficient * hashtable_.size()){
+                rehash(hashtable_.size() + 1);
+            }
+        }
+        return std::make_pair(new_place_it, success);
+    }
 
 	Iterator erase(const Iterator erase_it)
 	{
 		/*Erases an element pointed by erase_it
 		Complexity: constant*/
-		size_t erase_list_index = hash_function((*erase_it).first);
-		Iterator next_element_it = hashtable_[erase_list_index].erase(erase_it);
+        Iterator next_element_it = erase_it;
+        ++next_element_it;
+        bool last_element = (next_element_it == it_end_);
+        (*erase_it.bucket_it_).erase(erase_it.list_it_);
 		element_quantity_--;
 		update_begin();
 		update_end();
+        if (last_element){
+            next_element_it = it_end_;
+        }
 		return next_element_it;
 	}
 
@@ -164,10 +216,13 @@ public:
 	{
 		/*Erases an element by its key
 		Complexity: linear in the element_quantity_*/
-		Iterator it = find(key);
-		if (it != it_end_){
-			it = erase(it);
-		}
+        Iterator it(*this, it_end_);
+        if (!isEmpty()){
+            it = find(key);
+            if (it != it_end_){
+                it = erase(it);
+            }
+        }
 		return it;
 	}
 
@@ -179,28 +234,25 @@ public:
 			return;
 		} else {
 			if (new_hashtable_size > hashtable_.size()){
-                for (size_t new_bucket = hashtable_.size(); new_bucket < new_hashtable_size; ++new_bucket){
-                    //std::list<TListElement> empty_list;
-//                    hashtable_.push_back(std::list<TListElement>());
-                hashtable_.resize(new_hashtable_size); //hashtable_.size() has changed
-                }
 				/*run through all the elements and reallocate them*/
-				update();
+                update(new_hashtable_size);
 			}
 			/*else do nothing*/
 		}
-		update_begin();
-		update_end();
 	}
 
-	ValueT& operator [](KeyT& key)
+    ValueT& operator[](KeyT& key)
 	{
 		Iterator it = find(key);
 		return (*it);
 	}
 
+    size_t size()
+    {
+        return element_quantity_;
+    }
+
 private:
-	/*size_t hash_function(KeyT key);*/
 
 	size_t hash_function(const KeyT& key) const
 	{
@@ -209,38 +261,55 @@ private:
 
 	void update_begin()
 	{
-        for (it_begin_.list_it_ = hashtable_.front().begin(); it_begin_.list_it_ != hashtable_.back().end(); ++it_begin_){
-			if (it_begin_.list_it_ != it_begin_.bucket_it_->end()){
-				break;
-			}
-		}
+        if (isEmpty()){
+            it_begin_.bucket_it_ = hashtable_.begin();
+            it_begin_.list_it_ = hashtable_.front().begin();
+        }
+        else {
+            for (size_t index = 0; index < hashtable_.size(); ++index){
+                if (!hashtable_[index].empty()){
+                    it_begin_.bucket_it_ = hashtable_.begin() + index;
+                    it_begin_.list_it_ = hashtable_[index].begin();
+                    break;
+                }
+            }
+        }
 	}
 
 	void update_end()
 	{
-		for (it_end_.list_it_ = hashtable_.back().end(); it_end_.list_it_ != hashtable_.front().begin(); --it_end_){
-			if (it_end_.list_it_ != it_end_.bucket_it_->begin()){
-				break;
-			}
-		}
+        if (isEmpty()){
+            it_end_ = it_begin_;
+        }
+        else {
+            for (ssize_t index = hashtable_.size() - 1; index >= 0; --index){
+                if (!hashtable_[index].empty()){
+                    it_end_.bucket_it_ = hashtable_.begin() + index;
+                    it_end_.list_it_ = hashtable_[index].end();
+                    break;
+                }
+            }
+        }
     }
 
-	void update()
+    void update(size_t new_hashtable_size)
 	{
 		/*Updates the hashtable_ after the increase of number of buckets
 		Complexity: linear in the element_quantity_*/
-		std::vector< std::list <TListElement> > new_hashtable;
-		std::swap(hashtable_, new_hashtable);
+        HashTable<KeyT, ValueT> new_hashtable(new_hashtable_size);
 		
-		element_quantity_ = 0;
-		for (Iterator it = it_begin_; it != it_end_; ++it){
-			insert(it->first, it->second);
+        Iterator it(*this, it_begin_);
+        for (; it != it_end_; ++it){
+            new_hashtable.insert(it->first, it->second);
 		}
+        std::swap(*this, new_hashtable);
+        update_begin();
+        update_end();
     }
 
-    size_t element_quantity_;
     std::vector< std::list <TListElement> > hashtable_;
     Iterator it_begin_;
     Iterator it_end_;
-	static const size_t rehash_coefficient = 4;
+    size_t element_quantity_;
+    static const size_t rehash_coefficient = 4;
 };
--- 1t5p_hashtable/main.cpp
+++ 1t5p_hashtable/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <string>
 #include "hashtable.h"
-const size_t bucket_quantity = 2;
+const size_t bucket_quantity = 1;
 
 int main()
 {
 	HashTable<std::string, std::string> hashtable(bucket_quantity);
-    hashtable.insert(std::string("1"), std::string("one"));
-	
-	return 0;
+    std::string zero("0");
+    std::string zero1("zero");
+    std::string one("1");
+    std::string one1("one");
+    std::string two("22");
+    std::string two1("two");
+    std::string three("333");
+    std::string three1("three");
+    std::cout << hashtable.insert(zero, zero1).second << "\n";
+    std::cout << hashtable.insert(one, one1).second << "\n";
+    std::cout << hashtable.insert(two, two1).second << "\n";
+    std::cout << hashtable.insert(three, three1).second << "\n"; //Here will be rehash
+    std::cout << "Inserting " << hashtable.size() << " elements: Finished!\n";
+
+    HashTable<std::string, std::string>::Iterator element_it = hashtable.find("0");
+    std::cout << (*element_it) << "\n";
+    std::cout << hashtable[one] << "\n";
+
+    std::cout << "List of all elements: \n";
+    for (HashTable<std::string, std::string>::Iterator it = hashtable.begin(); it != hashtable.end(); ++it){
+        std::cout << it->first << " : " << it->second << "\n";
+    }
+
+    element_it = hashtable.erase(zero);
+    std::cout << "After zero goes: " << element_it->first << " - " << element_it->second << "\n";
+    std::cout << "CHECKPOINT\n";
+    return 0;
 }
