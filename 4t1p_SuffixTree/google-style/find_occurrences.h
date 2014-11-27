162e5810fb9a311f106178dea7991d8a59754715
Andrew <sautin1@yandex.ru>
2014-11-28 01:14:13 +0300

    12. applied googly style naming rules (no formatting yet)

--- 4t1p_SuffixTree/google-style/find_occurrences.h
+++ 4t1p_SuffixTree/google-style/find_occurrences.h
@@ -1,3 +1,12 @@
+// Copyright 2014, MIPT
+// All rights reserved.
+//
+// Author: sautin1@yandex.ru (Andrew Sautin)
+//
+// The Suffix Tree C++ Library
+//
+// This header file declares the public API for searching substring occurrences in a string.
+
 #ifndef SUBSTRINGS_H
 #define SUBSTRINGS_H
 
@@ -7,16 +16,11 @@
 
 typedef SuffixTree::LinkMapConstIterator LinkMapConstIterator;
 
+// TraversalVisitor class for SuffixTree.DepthFirstSearchTraversal() method.
+// Is designed for the needs of FindAllOccurences() function.
 class FindOccurrencesTraversalVisitor {
-private:
-	const SuffixTree& suffix_tree;
-	std::string pattern;
-	std::vector<int> occurrences;
-	int pattern_index;
-	int pattern_end_node;
-	int suffix_length;
 public:
-	FindOccurrencesTraversalVisitor(const SuffixTree& _suffix_tree, const std::string& _pattern);
+	FindOccurrencesTraversalVisitor(const SuffixTree& suffix_tree, const std::string& pattern);
 
 	void InitVisitor();
 	void DiscoverNode(const SuffixTree::Link& in_link);
@@ -26,9 +30,18 @@ public:
 			const LinkMapConstIterator& link_map_next_letter_it, const LinkMapConstIterator& link_map_end_it);
 	void FinishNode(const SuffixTree::Link& in_link);
 
-	std::vector<int> GetOccurrences() const;
+	const std::vector<int>& occurrences() const;
+private:
+	const SuffixTree& suffix_tree_;
+	std::string pattern_;
+	std::vector<int> occurrences_;
+	int pattern_index_;
+	int pattern_end_node_;
+	int suffix_length_;
 };
 
+// Returns vector of integers, that represent indices in suffix_tree sample,
+// where search_string starts as a substring.
 std::vector<int> FindAllOccurrences(const SuffixTree& suffix_tree, const std::string& search_string);
 
 #endif // SUBSTRINGS_H
