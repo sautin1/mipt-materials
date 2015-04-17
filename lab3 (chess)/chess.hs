015ce4499231c2fdb0440963da075d9fe1ae5709
Andrew <sautin1@yandex.ru>
2015-04-17 12:57:48 +0300

    1 minute before realizing that I should have used ByteString

--- lab3 (chess)/chess.hs
+++ lab3 (chess)/chess.hs
@@ -4,24 +4,25 @@ import System.IO
 import System.Environment
 import qualified Data.Char as Char
 import qualified Data.Map as Map
+import qualified Data.List as List
+import qualified Data.Trie as Trie
+import qualified Data.ByteString as B
 
 type Turn = String
 type Game = ([Turn], Bool) -- (turns, whiteWins)
-type Opening = String -- concatenation of turns-strings intercalated by space
+type Opening = B.ByteString -- concatenation of turns-strings intercalated by space
+type OpeningData = (Opening, Bool) -- opening + whiteWins
+type TrieNodeInfo = (Integer, Bool)
 
 buildGame :: String -> Game
-buildGame s = (turnsString, gameResult)
+buildGame s = (turnsStr, gameResult)
     where
         (rawTurns, rawResult) = break (\x -> head x == '{') $ words s
-        turnsString = filter (\x -> Char.isLetter $ head x) rawTurns
+        turnsStr = filter (\x -> Char.isLetter $ head x) rawTurns
         gameResult = head (last rawResult) == '1'
 
-generateOpenings :: Game -> [Opening]
-generateOpenings = (scanl1 (\x y -> x ++ (' ':y)) . fst)
--- without lambda: flip ((flip (++)).(' ':))
-
-createOpeningCounter :: [Opening] -> Map.Map Opening Integer
-createOpeningCounter = Map.fromList . map (\x -> (x, 1))
+createTrieNodeInfo :: [OpeningData] -> [(Opening, TrieNodeInfo)]
+createTrieNodeInfo = map (\x -> (fst x, (1, snd x)))
 
 main = do
     args <- getArgs
@@ -29,11 +30,10 @@ main = do
     contents <- hGetContents handle
     let usefulData = filter (\s -> (s /= "\r") && (head s /= '[')) $ lines contents
     let gameSet = map buildGame usefulData
-    let inCounter = map (createOpeningCounter . generateOpenings) gameSet
-    --print $ length inCounter
-    --putStrLn "___"
-    mapM_ print inCounter
-    let counter = Map.filter (> 1) $ Map.unionsWith (+) inCounter
-    print $ Map.size counter
-    --mapM_ print $ take 10 $ Map.toList counter
+    let openings = map (List.intercalate " " . fst) gameSet
+    let openingDataList = zip openings $ map snd gameSet
+    let trie = Trie.fromList $ createTrieNodeInfo openingDataList
+    --print Trie.member 
+    --let counter = Map.filter (> 1) $ Map.unionsWith (+) inCounter
+    --print $ Map.size counter
     hClose handle
