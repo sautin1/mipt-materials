690cc8502fde233818b446fac68461fac34e8d6b
Andrew <sautin1@yandex.ru>
2015-05-15 21:40:26 +0300

    graph construction completed

--- lab4 (scheme)/lab4.hs
+++ lab4 (scheme)/lab4.hs
@@ -3,6 +3,7 @@
 import System.IO
 import Data.Graph
 import qualified Data.Set as Set
+import Data.List (isInfixOf, isSuffixOf, dropWhileEnd)
 import qualified Data.Text as T
 import Network.URL
 import Network.HTTP.Conduit hiding (host)
@@ -22,21 +23,25 @@ createURL = fromJust . importURL . T.unpack
 resolveURL :: URL -> URL -> URL
 resolveURL base url = case (url_type base, url_type url) of
     (_, Absolute _) -> url
-    (Absolute h, _) -> URL {
+    (Absolute h, HostRelative) -> URL {
+        url_type = url_type base,
+        url_path = url_path url,
+        url_params = url_params url
+    }
+    (Absolute h, PathRelative) -> URL {
             url_type = url_type base,
-            url_path = url_path url,
+            url_path = (dropWhileEnd (/= '/') $ url_path base) ++ url_path url,
             url_params = url_params url
         }
     otherwise -> url
 
-makeRelative :: URL -> URL
-makeRelative url = case url_type url of
-    HostRelative -> url
-    otherwise -> URL {
-            url_type = HostRelative,
-            url_path = url_path url,
-            url_params = url_params url
-        }
+makeHostRelative :: URL -> URL -> URL
+makeHostRelative base url = URL {
+        url_type = HostRelative,
+        url_path = url_path resolvedUrl,
+        url_params = url_params resolvedUrl
+    }
+        where resolvedUrl = resolveURL base url
 
 hasSameDomain :: URL -> URL -> Bool
 hasSameDomain base url = case (url_type base, url_type url) of 
@@ -45,47 +50,78 @@ hasSameDomain base url = case (url_type base, url_type url) of
     (Absolute bh, Absolute uh) -> bh == uh
     otherwise -> False
 
+isMailto :: URL -> Bool
+isMailto = (isInfixOf "mailto:") . url_path
+
+isGoodUrl :: URL -> URL -> Bool
+isGoodUrl base url = hasSameDomain base url && not (isMailto url)
+    && and (map (\ext -> not $ ext `isSuffixOf` urlPath) [  ".png", ".jpg", ".gif", ".jpeg", ".pdf", 
+                                                            ".doc", ".rtf", ".xls", ".pptx", ".pps", 
+                                                            ".zip", ".rar", ".mp3"])
+        where urlPath = url_path url
+
 relatedLinks :: URL -> IO (Set.Set URL)
 relatedLinks url = do
     cursor <- cursorFor url
     let textLinks = cursor  $// laxElement "a"
                             >=> hasAttribute "href"
-                            >=> check ((hasSameDomain url) . createURL . (T.concat) . attribute "href")
+                            >=> check ((isGoodUrl url) . createURL . (T.concat) . attribute "href")
                             >=> attribute "href"
-    return $ foldl (\set link -> Set.insert (makeRelative $ createURL link) set) Set.empty textLinks
+    return $ foldl (\set link -> Set.insert (makeHostRelative url $ createURL link) set) Set.empty textLinks
 
 referencesClosure :: URL -> IO [(URL, [URL])]
-referencesClosure rootUrl = referencesClosure' [makeRelative rootUrl] (Set.singleton $ makeRelative rootUrl)
+referencesClosure rootUrl = referencesClosure' [relativeRootUrl] (Set.singleton $ relativeRootUrl)
     where
+        relativeRootUrl = makeHostRelative rootUrl rootUrl
+
         referencesClosure' [] _ = return []
         referencesClosure' (url : queue) set = do
+            putStrLn $ "\n\tParsing " ++ exportURL url
             linkSet <- relatedLinks $ resolveURL rootUrl url
             let links = Set.elems linkSet
-            putStrLn "\n\tTEST"
-            print url
-            putStrLn "\tRefs:"
-            mapM_ print links
-            putStrLn "__"
             let addSet = Set.difference linkSet set
             let newSet = Set.union set addSet
             let newLinks = Set.elems addSet
-            putStrLn "\tnew:"
-            mapM_ print newLinks
             let newQueue = queue ++ newLinks
+            putStr $ "\tQueue: "
+            print $ length newQueue
             sonRes <- referencesClosure' newQueue newSet
+            --print (url, links)
+            --putStrLn ""
             return $ (url, links) : sonRes
 
-webGraph :: URL -> IO ()--IO Graph
-webGraph url = do
-    references <- referencesClosure url
-    mapM_ (print . fst) references
-    return ()
+pageRank :: (Graph, Vertex -> (node, key, [key]), key -> Maybe Vertex) -> (Graph, Int)
+pageRank (gr, unhash, hash) = (gr, 0)
+--  | 
+--      where (newGr, maxDiff) = pageRankIter gr
+--          where 
+--              pageRankIter 
+--                  where
+--                      dampFact = 0.85
+--                      eps = 0.5
+
 
 main :: IO()
 main = do
+    putStrLn "Enter link: "
     --link <- getLine
-    --let link = "http://progmeistars.lv/index.php?lang=ru" -- some kind of error from server
     let link = "http://www.alieparusa.ru/"
-    graph <- webGraph $ createURL link
+    --let link = "http://student.progmeistars.lv/2009_06Alexey%20'popoffka'%20Popov/"
+    --let link = "http://www.drive2.ru/" -- works too long since the site is enormous. Looks spectacular.
+    --let link = "http://lio.lv" -- fails because of "..\..\default.htm"
+    --let link = "http://progmeistars.lv/index.php?lang=ru" -- fails because of "/../olimpics.html"
+    
+    putStrLn "Building web graph..."
+    references <- referencesClosure $ createURL $ T.pack link
+    let forGraph = zipWith (\(l, ls) node -> (node, l, ls)) references [0,0..]
+    let grRes@(webGraph, unhash, hash) = graphFromEdges forGraph
+    putStr "... web graph of "
+    putStr $ show $ length $ vertices webGraph
+    putStr " nodes and "
+    putStr $ show $ length $ edges webGraph
+    putStrLn " edges is built!"
+
+    putStrLn "Counting PageRank..."
+    let prGraph = pageRank grRes
+    putStrLn "... finished!"
     return ()
-    --let webGraph = graphFromEdges graphList
