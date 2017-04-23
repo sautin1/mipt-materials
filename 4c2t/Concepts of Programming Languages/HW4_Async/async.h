8ab251339e417238f14774f1b631938069080c34
Andrew Sautin <sautin1@yandex.ru>
2017-04-23 23:16:16 +0300

    Add synchronization

--- 4c2t/Concepts of Programming Languages/HW4_Async/async.h
+++ 4c2t/Concepts of Programming Languages/HW4_Async/async.h
@@ -2,20 +2,27 @@
 
 #include <exception>
 #include <memory>
+#include <mutex>
+#include <thread>
 
 template<typename T>
 class CFuture {
 public:
     CFuture(const std::shared_ptr<std::shared_ptr<T>>& _value,
-            const std::shared_ptr<std::shared_ptr<std::exception>>& _exception)
+            const std::shared_ptr<std::shared_ptr<std::exception>>& _exception,
+            const std::shared_ptr<std::mutex> _mutex)
         : value(_value),
-          exception(_exception) {}
+          exception(_exception),
+          mutex(_mutex) {}
 
     std::shared_ptr<T> Get() const;
     std::shared_ptr<T> TryGet() const;
 private:
+    std::shared_ptr<T> getOrFail() const;
+
     std::shared_ptr<std::shared_ptr<T>> value;
     std::shared_ptr<std::shared_ptr<std::exception>> exception;
+    std::shared_ptr<std::mutex> mutex;
 };
 
 template<typename T>
@@ -23,7 +30,10 @@ class CPromise {
 public:
     CPromise()
         : value(new std::shared_ptr<T>()),
-          exception(new std::shared_ptr<std::exception>()) {}
+          exception(new std::shared_ptr<std::exception>()),
+          mutex(new std::mutex()) {
+        mutex->lock();
+    }
 
     void SetValue(const T& _value);
     void SetException(const std::exception& _exception);
@@ -31,16 +41,29 @@ public:
 private:
     std::shared_ptr<std::shared_ptr<T>> value;
     std::shared_ptr<std::shared_ptr<std::exception>> exception;
+    std::shared_ptr<std::mutex> mutex;
 };
 
 template<typename T>
 std::shared_ptr<T> CFuture<T>::Get() const {
-    // wait
-    return TryGet();
+    mutex->lock();
+    std::shared_ptr<T> result = getOrFail();
+    mutex->unlock();
+    return result;
 }
 
 template<typename T>
 std::shared_ptr<T> CFuture<T>::TryGet() const {
+    std::shared_ptr<T> result = nullptr;
+    if (mutex->try_lock()) {
+        result = getOrFail();
+        mutex->unlock();
+    }
+    return result;
+}
+
+template<typename T>
+std::shared_ptr<T> CFuture<T>::getOrFail() const {
     if (*exception) {
         throw **exception;
     }
@@ -50,14 +73,16 @@ std::shared_ptr<T> CFuture<T>::TryGet() const {
 template<typename T>
 void CPromise<T>::SetValue(const T& _value) {
     *value = std::make_shared<T>(_value);
+    mutex->unlock();
 }
 
 template<typename T>
 void CPromise<T>::SetException(const std::exception& _exception) {
     *exception = std::make_shared<std::exception>(_exception);
+    mutex->unlock();
 }
 
 template<typename T>
 CFuture<T> CPromise<T>::GetFuture() const {
-    return CFuture<T>(value, exception);
+    return CFuture<T>(value, exception, mutex);
 }
--- 4c2t/Concepts of Programming Languages/HW4_Async/main.cpp
+++ 4c2t/Concepts of Programming Languages/HW4_Async/main.cpp
@@ -1,5 +1,22 @@
+#include <chrono>
+#include <iostream>
 #include "async.h"
 
 int main() {
+    std::shared_ptr<CPromise<int>> promise(new CPromise<int>());
+    CFuture<int> future = promise->GetFuture();
+
+    auto work = [promise](int workDuration) {
+        std::cout << "Slave: working" << std::endl;
+        std::this_thread::sleep_for(std::chrono::microseconds(workDuration));
+        promise->SetValue(23);
+    };
+    std::thread thread(work, 1000);
+    thread.detach();
+
+//    std::cout << future.TryGet() << std::endl;
+    std::cout << "Master: Waiting" << std::endl;
+    int result = *(future.Get());
+    std::cout << result << std::endl;
     return 0;
 }
