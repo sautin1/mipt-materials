25b0761939fab35d122a1ec9559022bb09d2c037
Andrew Sautin <sautin1@yandex.ru>
2017-10-31 03:17:19 +0300

    Cache some values to speed up

--- 5c1t/Image Analysis (ABBYY)/hw1_demosaicing/demosaicing.py
+++ 5c1t/Image Analysis (ABBYY)/hw1_demosaicing/demosaicing.py
@@ -11,6 +11,8 @@ def _get_pixel_by_offset(matrix, config, offset):
 
 
 class DemosaicingVNG:
+    center = np.array([2, 2])
+
     def __init__(self, k1=1.5, k2=0.5):
         self.k1 = k1
         self.k2 = k2
@@ -63,15 +65,16 @@ class DemosaicingVNG:
 
     @staticmethod
     def _interpolate_green_pixel_by_directions(matrix, config, directions):
-        c = np.array([2, 2])  # center
+        c = DemosaicingVNG.center  # center
         get_pixel = partial(_get_pixel_by_offset, matrix, config)
         sums = np.array([0, 0, 0])  # red sum, green sum, blue sum
         for d in directions:
+            step_from_center = c + d
             if d[0] == 0 or d[1] == 0:
-                channel_in_direction = get_channel(config, *(c + d))
+                channel_in_direction = get_channel(config, *step_from_center)
                 channel_orthogonal = 2 - channel_in_direction
                 d_orth = d[::-1]  # orthogonal direction
-                sums[channel_in_direction] += get_pixel(c + d)
+                sums[channel_in_direction] += get_pixel(step_from_center)
                 for offset in [d_orth, -d_orth, d_orth + 2 * d, -d_orth + 2 * d]:
                     sums[channel_orthogonal] += get_pixel(c + offset) / 4
             else:
@@ -83,30 +86,31 @@ class DemosaicingVNG:
                 sums[channel_vertical] += get_pixel(vert + (0, 2 * d[1])) / 2
                 sums[channel_horizontal] += get_pixel(hor) / 2
                 sums[channel_horizontal] += get_pixel(hor + (2 * d[0], 0)) / 2
-                sums[1] += get_pixel(c + d)
-        diffs = (sums - sums[1]) / len(directions)
+                sums[1] += get_pixel(step_from_center)
+        diffs = (sums - sums[1]) / len(directions) if directions else sums
         return diffs + get_pixel(c)
 
     @staticmethod
     def _interpolate_non_green_pixel_by_directions(matrix, config, directions):
-        c = np.array([2, 2])  # center
+        c = DemosaicingVNG.center  # center
+        get_pixel = partial(_get_pixel_by_offset, matrix, config)
         channel_center = config[0, 0]
         channel_diagonal = 2 - channel_center
-        get_pixel = partial(_get_pixel_by_offset, matrix, config)
         sums = np.array([0, 0, 0])  # red sum, green sum, blue sum
         for d in directions:
+            step_from_center = c + d
             if d[0] == 0 or d[1] == 0:
                 d_orth = d[::-1]  # orthogonal direction
-                sums[channel_center] += (get_pixel(c + 2 * d) + get_pixel(c)) / 2
-                sums[channel_diagonal] += get_pixel(c + d + d_orth) / 2
-                sums[channel_diagonal] += get_pixel(c + d - d_orth) / 2
-                sums[1] += get_pixel(c + d)
+                sums[channel_center] += (get_pixel(step_from_center + d) + get_pixel(c)) / 2
+                sums[channel_diagonal] += get_pixel(step_from_center + d_orth) / 2
+                sums[channel_diagonal] += get_pixel(step_from_center - d_orth) / 2
+                sums[1] += get_pixel(step_from_center)
             else:
-                sums[channel_center] += (get_pixel(c + 2 * d) + get_pixel(c)) / 2
-                sums[channel_diagonal] += get_pixel(c + d)
+                sums[channel_center] += (get_pixel(step_from_center + d) + get_pixel(c)) / 2
+                sums[channel_diagonal] += get_pixel(step_from_center)
                 for offset in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-                    sums[1] += get_pixel(c + d + offset) / 4
-        diffs = (sums - sums[channel_center]) / len(directions)
+                    sums[1] += get_pixel(step_from_center + offset) / 4
+        diffs = (sums - sums[channel_center]) / len(directions) if directions else sums
         return diffs + get_pixel(c)
 
 
@@ -138,14 +142,15 @@ class GradientCalculator:
         is_hor = direction[0] == 0  # is horizontal
         m5 = c + ((-1, 0) if is_hor else (0, -1))  # aux for component#5
         m6 = c + ((1, 0) if is_hor else (0, 1))  # aux for component#6
-        components = np.abs(np.array([
+        d_double = d * 2
+        components = np.abs([
             get_pixel(c + d) - get_pixel(c - d),
-            get_pixel(c + 2 * d) - get_pixel(c),
+            get_pixel(c + d_double) - get_pixel(c),
             get_pixel(c + ((-1, 1) if is_hor else (1, -1))) - get_pixel(c + (-1, -1)),
             get_pixel(c + (1, 1)) - get_pixel(c + ((1, -1) if is_hor else (-1, 1))),
-            get_pixel(m5 + 2 * d) - get_pixel(m5),
-            get_pixel(m6 + 2 * d) - get_pixel(m6)
-        ]))
+            get_pixel(m5 + d_double) - get_pixel(m5),
+            get_pixel(m6 + d_double) - get_pixel(m6)
+        ])
         return np.sum(components[:2]) + np.sum(components[2:]) / 2
 
     @staticmethod
@@ -155,13 +160,13 @@ class GradientCalculator:
 
         m3 = c + ((0, -1) if d[1] == 1 else (0, 1))  # aux for component#3
         m4 = c + ((-1, 0) if d[0] == 1 else (1, 0))  # aux for component#4
-
-        components = np.abs(np.array([
+        d_double = d * 2
+        components = np.abs([
             get_pixel(c + d) - get_pixel(c - d),
-            get_pixel(c + 2 * d) - get_pixel(c),
-            get_pixel(m3 + 2 * d) - get_pixel(m3),
-            get_pixel(m4 + 2 * d) - get_pixel(m4)
-        ]))
+            get_pixel(c + d_double) - get_pixel(c),
+            get_pixel(m3 + d_double) - get_pixel(m3),
+            get_pixel(m4 + d_double) - get_pixel(m4)
+        ])
         return np.sum(components)
 
     @staticmethod
@@ -172,14 +177,14 @@ class GradientCalculator:
         m5 = c + (-1, 0)  # aux for component#5
         m6 = c + (1, 0)  # aux for component#6
 
-        components = np.abs(np.array([
+        components = np.abs([
             get_pixel(c + d) - get_pixel(c - d),
             get_pixel(c + 2 * d) - get_pixel(c),
-            get_pixel(c + (0, d[0] * d[1])) - get_pixel(c + (-1, 0)),
-            get_pixel(c + (1, 0)) - get_pixel(c + (0, -d[0] * d[1])),
+            get_pixel(c + (0, d[0] * d[1])) - get_pixel(m5),
+            get_pixel(m6) - get_pixel(c + (0, -d[0] * d[1])),
             get_pixel(m5 + d * (2 if d[0] == 1 else 1)) - get_pixel(m5 + d * (1 if d[0] == 1 else 0)),
             get_pixel(m6 + d * (2 if d[0] == -1 else 1)) - get_pixel(m6 + d * (1 if d[0] == -1 else 0)),
-        ]))
+        ])
         return np.sum(components[:2]) + np.sum(components[2:]) / 2
 
 if __name__ == '__main__':
@@ -187,7 +192,6 @@ if __name__ == '__main__':
     from os.path import join
 
     image = cv2.imread(join('images', 'RGB_CFA_300.bmp'), cv2.IMREAD_COLOR)
-    # image_interpolated = image[1300:1600, 1800:2100, :]
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image_interpolated = DemosaicingVNG().demosaic(image)
     image_interpolated = cv2.cvtColor(image_interpolated, cv2.COLOR_RGB2BGR)
