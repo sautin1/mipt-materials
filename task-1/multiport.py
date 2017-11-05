7298596f07d21b9e4a43abf945ee945a3743b92f
Andrew Sautin <sautin1@yandex.ru>
2017-11-05 20:03:42 +0300

    Add solution for 3 inputs

--- task-1/multiport.py
+++ task-1/multiport.py
@@ -43,8 +43,8 @@ class NotNode(Node):
         return ~args[0] & self.max_value
 
 
-class Multiport2Inputs:
-    INPUT_COUNT = 2
+class Multiport3Inputs:
+    INPUT_COUNT = 3
     FUNCTION_ID_LENGTH = 2 ** INPUT_COUNT
     MAX_FUNCTION_ID = 2 ** FUNCTION_ID_LENGTH - 1
     Edge = namedtuple('Edge', ['source', 'target'])
@@ -59,7 +59,7 @@ class Multiport2Inputs:
 
     def _build(self):
         # add inputs
-        input_function_ids = [0b1100, 0b1010]  # ##
+        input_function_ids = [0b11110000, 0b11001100, 0b10101010]
         input_node_ids = []
         for i, function_id in enumerate(input_function_ids):
             input_node_id = self._add_node(InputNode(), function_id)
@@ -67,29 +67,32 @@ class Multiport2Inputs:
 
         # add input negations
         for input_node_id in input_node_ids:
-            not_node = NotNode(Multiport2Inputs.MAX_FUNCTION_ID)
+            not_node = NotNode(Multiport3Inputs.MAX_FUNCTION_ID)
             not_node_function_id = not_node.execute(self.node_id_to_function_id[input_node_id])
             not_node_id = self._add_node(not_node, not_node_function_id)
-            self.edges.append(Multiport2Inputs.Edge(input_node_id, not_node_id))
+            self.edges.append(Multiport3Inputs.Edge(input_node_id, not_node_id))
         
         # add and's
-        nodes_single_one = set()
-        node_ids = list(self.nodes.keys())
-        for node_id_first in node_ids[:-1]:
-            for node_id_second in node_ids[node_id_first + 1:]:
-                and_node = AndNode()
-                function_id = and_node.execute(self.node_id_to_function_id[node_id_first],
-                                               self.node_id_to_function_id[node_id_second])
-                if function_id not in self.function_id_to_node_id:
-                    and_node_id = self._add_node(and_node, function_id)
-                    self.edges.append(Multiport2Inputs.Edge(node_id_first, and_node_id))
-                    self.edges.append(Multiport2Inputs.Edge(node_id_second, and_node_id))
-                    if function_id > 0:
-                        nodes_single_one.add(and_node_id)
+        layer = list(self.nodes.keys())
+        for _ in range(Multiport3Inputs.INPUT_COUNT - 1):
+            layer_new = set()
+            for idx, node_id_first in enumerate(layer[:-1]):
+                for node_id_second in layer[idx + 1:]:
+                    and_node = AndNode()
+                    function_id = and_node.execute(self.node_id_to_function_id[node_id_first],
+                                                   self.node_id_to_function_id[node_id_second])
+                    if function_id not in self.function_id_to_node_id:
+                        and_node_id = self._add_node(and_node, function_id)
+                        self.edges.append(Multiport3Inputs.Edge(node_id_first, and_node_id))
+                        self.edges.append(Multiport3Inputs.Edge(node_id_second, and_node_id))
+                        if function_id > 0:
+                            layer_new.add(and_node_id)
+            layer = list(layer_new)
+        nodes_single_one = layer
 
         # add or's
         layer = set(nodes_single_one)
-        for one_count in range(1, Multiport2Inputs.FUNCTION_ID_LENGTH):
+        for one_count in range(1, Multiport3Inputs.FUNCTION_ID_LENGTH):
             layer_new = set()
             for node_id in layer:
                 for node_id_single_one in nodes_single_one:
@@ -101,8 +104,8 @@ class Multiport2Inputs:
                                                   self.node_id_to_function_id[node_id_single_one])
                     if function_id not in self.function_id_to_node_id:
                         or_node_id = self._add_node(or_node, function_id)
-                        self.edges.append(Multiport2Inputs.Edge(node_id, or_node_id))
-                        self.edges.append(Multiport2Inputs.Edge(node_id_single_one, or_node_id))
+                        self.edges.append(Multiport3Inputs.Edge(node_id, or_node_id))
+                        self.edges.append(Multiport3Inputs.Edge(node_id_single_one, or_node_id))
                     layer_new.add(self.function_id_to_node_id[function_id])
             layer = layer_new
 
@@ -121,7 +124,7 @@ class Multiport2Inputs:
 
 
 if __name__ == '__main__':
-    multiport = Multiport2Inputs()
+    multiport = Multiport3Inputs()
     for edge in multiport.get_edges():
         print('{} {}'.format(edge.source, edge.target))
     for node_id, node in multiport.get_nodes().items():
