91ee6c6f9557a122662b407110a1f0bf3afa5626
Andrew Sautin <sautin1@yandex.ru>
2017-11-10 18:48:04 +0300

    Add determinant calculation by modulo

--- task-2/matching.py
+++ task-2/matching.py
@@ -4,8 +4,29 @@ import numpy as np
 def has_perfect_matching(incidence_matrix, randint_max=int(10 ** 9) + 7):
     weights = np.random.randint(1, randint_max, size=incidence_matrix.shape)
     matrix = incidence_matrix * weights
-    sign, _ = np.linalg.slogdet(matrix)
-    return sign != 0
+    return calculate_determinant(matrix) != 0
+
+
+def calculate_determinant(matrix, modulo=int(10 ** 9) + 7):
+    det = 1
+    matrix %= modulo
+    for pivot_col in range(matrix.shape[1]):
+        pivot_row = np.argmax(matrix[pivot_col:, pivot_col]) + pivot_col
+        pivot = int(matrix[pivot_row, pivot_col])
+        if pivot == 0:
+            det = 0
+            break
+        if pivot_row != pivot_col:
+            matrix[[pivot_row, pivot_col]] = matrix[[pivot_col, pivot_row]]
+            det = modulo - det
+            pivot_row = pivot_col
+        pivot_inverse = pow(pivot, modulo - 2, modulo)
+        det = (det * pivot) % modulo
+        matrix[pivot_row] = (matrix[pivot_row] * pivot_inverse) % modulo
+        for row in range(pivot_row + 1, matrix.shape[0]):
+            matrix[row] += matrix[pivot_row] * (modulo - matrix[row, pivot_col])
+            matrix[row] %= modulo
+    return det
 
 
 def build_incidence_matrix(edges):
