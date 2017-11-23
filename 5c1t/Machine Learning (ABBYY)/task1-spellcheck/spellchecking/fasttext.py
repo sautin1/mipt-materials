05bc2f5c547801eab4548d516c90fc07c9d354a7
Andrew Sautin <sautin1@yandex.ru>
2017-11-23 15:18:37 +0300

    WIP: spellchecking/fasttext

--- 5c1t/Machine Learning (ABBYY)/task1-spellcheck/spellchecking/fasttext.py
+++ 5c1t/Machine Learning (ABBYY)/task1-spellcheck/spellchecking/fasttext.py
@@ -3,20 +3,22 @@ class SpellCheckerFastTextSimilarity:
         self.fasttext_model = fasttext_model
         self.similar_count = similar_count
         self.vocabulary = vocabulary
+        self.count = 0
 
     def correct(self, word):
         result = None
-        print(word)
+        # print(word)
         if word in self.fasttext_model.wv.vocab:
-            similar_words = self.fasttext_model.similar_by_word(word, topn=self.similar_count)
-            print(similar_words)
-            distances = dict(similar_words)
-            print(distances)
-            candidates = {word for word in similar_words if word in self.vocabulary}
-            print(candidates)
+            distances = dict(self.fasttext_model.similar_by_word(word, topn=self.similar_count))
+            # print(distances)
+            candidates = {word for word in distances if word in self.vocabulary}
+            # print(candidates)
             result = max(candidates, key=lambda candidate: distances[candidate] * self.vocabulary[candidate],
                          default=None)
-            print(result)
-        print('-' * 10)
-        _ = input()
+            # print(result)
+        # print('-' * 10)
+        # _ = input()
+        if result is None:
+            self.count += 1
+            print(count)
         return result or word
