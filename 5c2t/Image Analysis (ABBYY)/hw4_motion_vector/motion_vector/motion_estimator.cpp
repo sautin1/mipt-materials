f4d51b71ff09e2155418166c1264bab7cac91c02
Andrew Sautin <sautin1@yandex.ru>
2018-03-20 19:46:46 +0300

    Add motion vector filtration and replace vector<Vector> with Mat

--- 5c2t/Image Analysis (ABBYY)/hw4_motion_vector/motion_vector/motion_estimator.cpp
+++ 5c2t/Image Analysis (ABBYY)/hw4_motion_vector/motion_vector/motion_estimator.cpp
@@ -1,28 +1,27 @@
 #include "motion_estimator.h"
 
 Vector MotionEstimator::estimate_global(const Mat& image_current, const Mat& image_previous, bool show_vectors) const {
-    std::vector<Vector> local_motion_vectors = estimate_local(image_current, image_previous, show_vectors);
+    Mat local_motion_vectors = estimate_local(image_current, image_previous, show_vectors);
     double x_sum, y_sum;
-    for (const Vector& motion_vector : local_motion_vectors) {
-        x_sum += motion_vector.x;
-        y_sum += motion_vector.y;
-    }
-    x_sum /= local_motion_vectors.size();
-    y_sum /= local_motion_vectors.size();
-    return Vector(static_cast<int>(std::round(x_sum)), static_cast<int>(std::round(y_sum)));
+//    for (const Vector& motion_vector : local_motion_vectors) {
+//        x_sum += motion_vector.x;
+//        y_sum += motion_vector.y;
+//    }
+//    x_sum /= local_motion_vectors.size();
+//    y_sum /= local_motion_vectors.size();
+//    return Vector(static_cast<int>(std::round(x_sum)), static_cast<int>(std::round(y_sum)));
 }
 
-std::vector<Vector> MotionEstimator::estimate_local(const Mat& image_current, const Mat& image_previous,
+Mat MotionEstimator::estimate_local(const Mat& image_current, const Mat& image_previous,
                                                     bool show_vectors) const {
-    std::vector<Vector> motion_vectors;
+    Mat motion_vectors(image_current.rows / block_size, image_current.cols / block_size, CV_8SC2, Vec2i(0, 0));
     Mat image_to_display = image_current.clone();
-    motion_vectors.reserve((image_current.rows - block_size) * (image_current.cols - block_size));
     for (int row_start = 0; row_start < image_current.rows - block_size + 1; row_start += block_size) {
         for (int col_start = 0; col_start < image_current.cols - block_size + 1; col_start += block_size) {
             Point block_current_start(col_start, row_start);
             Point block_previous_start = find_closest_block(image_current, block_current_start, image_previous);
-            motion_vectors.emplace_back(block_current_start.x - block_previous_start.x,
-                                        block_current_start.y - block_previous_start.y);
+            motion_vectors.at<Vec2i>(row_start, col_start) = Vec2i(block_current_start.x - block_previous_start.x,
+                                                                   block_current_start.y - block_previous_start.y);
             if (show_vectors) {
                 rectangle(image_to_display,
                           block_current_start,
@@ -42,6 +41,10 @@ std::vector<Vector> MotionEstimator::estimate_local(const Mat& image_current, co
         waitKey(0);
     }
     return motion_vectors;
+}
+
+double MotionEstimator::calc_distance(const Mat& block_left, const Mat& block_right, NormTypes norm_type) const {
+    return norm(block_left, block_right, norm_type);
 }
 
 Point MotionEstimator::find_closest_block(const Mat& image_base,
@@ -64,7 +67,7 @@ Point MotionEstimator::find_closest_block(const Mat& image_base,
                 }
                 Rect block_search_rect(block_start.x + delta_col, block_start.y + delta_row,
                                        block_size, block_size);
-                double distance = calc_distance(block, image_to_search(block_search_rect));
+                double distance = calc_distance(block, image_to_search(block_search_rect), NORM_L2);
                 if (distance < min_distance) {
                     min_distance = distance;
                     point_best = Point(block_start.x + delta_col, block_start.y + delta_row);
@@ -81,6 +84,24 @@ Point MotionEstimator::find_closest_block(const Mat& image_base,
     return point_best;
 }
 
-double MotionEstimator::calc_distance(const Mat& block_left, const Mat& block_right) const {
-    return norm(block_left, block_right, NORM_L2);
+std::vector<Vector> MotionEstimator::filter_by_belief(const Mat& image_current, const std::vector<Vector>& motion_vectors) const {
+    std::vector<double> beliefs(motion_vectors.size(), 0);
+    std::vector<Vector> res;
+    res.reserve(motion_vectors.size());
+    int vector_idx = 0;
+    for (int row_start = 0; row_start < image_current.rows - block_size + 1; row_start += block_size) {
+        for (int col_start = 0; col_start < image_current.cols - block_size + 1; col_start += block_size) {
+            Rect block_rect(col_start, row_start, block_size, block_size);
+            beliefs[vector_idx] = calc_belief_function(motion_vectors[vector_idx], image_current(block_rect));
+            if (beliefs[vector_idx] >= belief_threshold) {
+                res.push_back(motion_vectors[vector_idx]);
+            }
+            ++vector_idx;
+        }
+    }
+    return res;
+}
+
+double MotionEstimator::calc_belief_function(const Vector& vector, const Mat& block) const {
+
 }
--- 5c2t/Image Analysis (ABBYY)/hw4_motion_vector/motion_vector/motion_estimator.h
+++ 5c2t/Image Analysis (ABBYY)/hw4_motion_vector/motion_vector/motion_estimator.h
@@ -14,16 +14,23 @@ struct Vector {
 
 class MotionEstimator {
 public:
-    MotionEstimator(int _block_size = 16, int step_size = 8)
-        : block_size(_block_size), initial_step_size(step_size) {}
+    MotionEstimator(int _block_size = 16, int step_size = 8, double _error_weight = 0.25,
+                    double _disp_weight = 32, double _dev_weight = 1, double _belief_threshold = 10)
+        : block_size(_block_size), initial_step_size(step_size), error_weight(_error_weight),
+          disp_weight(_disp_weight), dev_weight(_dev_weight), belief_threshold(_belief_threshold) {}
     Vector estimate_global(const Mat& image_current, const Mat& image_previous,
                            bool show_vectors = false) const;
-    std::vector<Vector> estimate_local(const Mat& image_current, const Mat& image_previous,
-                                       bool show_vectors = false) const;
+    Mat estimate_local(const Mat& image_current, const Mat& image_previous, bool show_vectors = false) const;
 private:
-    double calc_distance(const Mat& block_left, const Mat& block_right) const;
+    double calc_distance(const Mat& block_left, const Mat& block_right, NormTypes norm_type) const;
     Point find_closest_block(const Mat& image_base, Point block_start, const Mat& image_to_search) const;
+    std::vector<Vector> filter_by_belief(const Mat& image_current, const std::vector<Vector>& motion_vectors) const;
+    double calc_belief_function(const Vector& vector, const Mat& block) const;
 
-    int block_size;
-    int initial_step_size;
+    const int block_size;
+    const int initial_step_size;
+
+    // weights for belief function calculation
+    const double error_weight, disp_weight, dev_weight;
+    const double belief_threshold;
 };
